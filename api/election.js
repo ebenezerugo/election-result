8339906f080cfd732649a2f91fd8c2130900d87e
Ebenezer Ugo <ugo.ebenezer@gmail.com>
2020-03-15 00:24:05 +0100

    Fix issue

--- api/election.js
+++ api/election.js
@@ -88,7 +88,7 @@ module.exports = (app, db) => {
 
             // Aggregating all the polling units id together in to an array
             let polling_unit_uniqueIds = polling_units.map(polling_unit=>{
-                return polling_units.uniqueid;
+                return polling_unit.uniqueid;
             });
             console.log(polling_unit_uniqueids);
             // Use the aggregated polling unit id to get and sum announced polling unit result and group it to parties.
