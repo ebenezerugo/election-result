34b1467ad096b407beaa74425140927dc75d0670
Ebenezer Ugo <ugo.ebenezer@gmail.com>
2020-03-15 00:04:20 +0100

    Add features

--- api/election.js
+++ api/election.js
@@ -32,6 +32,80 @@ module.exports = (app, db) => {
             .then( (result) => res.status(200).json(result))
             .catch(error=>res.status(500).json({message:"Error occurred"}))
     });
+
+    app.get("/states", (req,res) => {
+        db.states.findAll()
+            .then(result=>res.status(200).json(result))
+            .catch(error=>res.status(500).json({message:"Error occurred"}));
+    });
+
+    app.get("/lga/:state_id", (req,res)=> {
+        const {state_id} = req.params;
+        db.lga.findAll({where:{state_id}})
+            .then(result=>res.status(200).json(result))
+            .catch(error=>res.status(500).json({message:"Error occurred"}));
+    });
+
+    app.get("wards/:lga_id", (req,res) => {
+        const {lga_id} = req.params;
+        db.ward.findAll({where:{lga_id}})
+        .map(async(result) => {
+            const {lga_id} = result;
+            const lga = await db.lga.findOne({where:{lga_id:lga_id}});
+            result.dataValues.lga = lga;
+            return result;
+        })
+        .then(result=>res.status(200).json(result))
+        .catch(error=>res.status(500).json({message:"Error occurred"}));
+    });
+
+    app.get( "/polling-units/:ward_id", (req, res) => {
+        const {ward_id} = req.params;
+        db.polling_unit.findAll({where:{ward_id}})
+          .map(async(result) => {
+              const {lga_id,uniquewardid} = result;
+              const ward = await db.ward.findOne({where:{uniqueid:uniquewardid}});
+              const lga = await db.lga.findOne({where:{lga_id:lga_id}});
+              result.dataValues.ward = ward;
+              result.dataValues.lga = lga;
+              return result;
+          })
+          .then(result=>res.status(200).json(result))
+          .catch(error=>{
+              console.log(error);
+              res.status(500).json({message:"Error occurred",error})
+          });
+      });
+
+    app.get("/total-polling-unit/:lga_id", async (req,res) => {
+        const {lga_id} = req.params;
+        try {
+            // Getting the polling units in a local goverment area.
+            let polling_units = await db.polling_unit.findAll({where:{lga_id}});
+            let total_polling_units_count = polling_units.length;
+
+            // Aggregating all the polling units id together in to an array
+            let polling_unit_uniqueIds = polling_units.map(polling_unit=>{
+                return polling_units.uniqueid;
+            });
+
+            // Use the aggregated polling unit id to get and sum announced polling unit result and group it to parties.
+            let parties_score = await db.announced_pu_results.findAll({
+                attributes: [
+                    'party_abbreviation',
+                    [sequelize.fn('sum', sequelize.col('party_score')), 'total_party_score'],
+                ],
+                group: ['party_abbreviation'],
+                where:{polling_unit_uniqueid:{[Op.or]: polling_unit_uniqueIds}}
+            });
+
+            res.status(200).json({total_polling_units_count,parties_score})
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({message:"Error occurred",error});
+        }
+      });
+
   
     // app.post("/post", (req, res) => 
     //   db.post.create({
