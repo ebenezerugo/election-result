ebeef9c29093eadc06445cf2fd96a383b6d5b101
Ebenezer Ugo <ugo.ebenezer@gmail.com>
2020-03-15 15:44:41 +0100

    Append lga

--- public/script/display-result.js
+++ public/script/display-result.js
@@ -48,7 +48,6 @@ function onStateChange(state_value) {
     let xhr = new XMLHttpRequest();
     xhr.open("GET", `${getAllLgaUrl}/${state_value}`);
     xhr.onload = function() {
-        lga.innerHTML = '<span>Hello</span>';
         let selectLga = `<select class="form-control" id="lgaInputField" onchange="onLgaChange">`;
         if(xhr.status == 200) {
             let data = JSON.parse(this.response);
@@ -64,6 +63,8 @@ function onStateChange(state_value) {
         }
         selectLga += `</select>`;
         lga.innerHTML = selectLga;
+        lga.innerHTML = '<span>Hello</span>';
+
     };
     xhr.send();
 }
